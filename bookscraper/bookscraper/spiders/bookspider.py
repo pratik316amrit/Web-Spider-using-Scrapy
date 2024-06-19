720b1c7538458aabe8d706a38234b0b744bdf59e
pratik316amrit <141550479+pratik316amrit@users.noreply.github.com>
2024-06-19 15:09:08 +0530

    Simple Spider programmed using Scrapy

---
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -0,0 +1,38 @@
+import scrapy
+import sys
+sys.path.append('D:/Tutorials/Scrapy/Practice/bookscraper/bookscraper')
+from items import BookItem
+
+class BookspiderSpider(scrapy.Spider):
+    name = 'bookspider'
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['https://books.toscrape.com/']
+
+    def parse(self, response):
+        books = response.css('article.product_pod')
+        for book in books:
+            relative_url = book.css('h3 a').attrib['href']
+            if 'catalogue/' in relative_url:
+                book_url = 'https://books.toscrape.com/' + relative_url
+            else:
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
+            yield scrapy.Request(book_url, callback=self.parse_book_page)   
+
+        ## Next Page        
+        next_page = response.css('li.next a ::attr(href)').get()
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page 
+            yield response.follow(next_page_url, callback=self.parse)
+
+    def parse_book_page(self, response):
+        book = response.css("div.product_main")[0]
+        table_rows = response.css("table tr")
+        book_item = BookItem()
+        
+         
+        
+
+        yield book_item
